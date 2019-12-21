b6487a4284b80fc0e7da390b8dfce45630cba415
LukMarks <lucas.mmoreno@hotmail.com>
2019-12-21 16:15:03 -0300

    fortran source code

--- fortran source/finite_elements.f95
+++ fortran source/finite_elements.f95
@@ -5,9 +5,11 @@
 program finiteElements
     implicit none
 
+    real :: angle                                           ![degree] temporary angle
     real :: alfa                                            ![degree] angle between two elements
-    !real,allocatable,dimension(:) :: nodes                  ![m](x,y) nodes coordenates
-    real, dimension(4,2) :: nodes
+    !real,allocatable,dimension(:) :: nodes                 ![m](x,y) nodes coordenates
+    real, dimension(4,2) :: nodes                           ![m](x,y) nodes coordenates
+    real:: L(3)                                             ![m] length of the element
     integer, parameter :: ikind=selected_real_kind(p=18)    !define precision 
     real(kind = ikind) :: E                                 ![N/m²] Young Modulus
     INTEGER :: row,col,max_rows,max_cols
@@ -21,6 +23,10 @@ program finiteElements
     close(2)
 
 
+    L(1) = sqrt((nodes(2,1)-nodes(1,1))**2+(nodes(2,2)-nodes(1,2))**2)
+    L(2) = sqrt((nodes(3,1)-nodes(2,1))**2+(nodes(3,2)-nodes(2,2))**2)
+    L(3) = sqrt((nodes(4,1)-nodes(3,1))**2+(nodes(4,2)-nodes(3,2))**2)
 
-    
+
+
 end program finiteElements
