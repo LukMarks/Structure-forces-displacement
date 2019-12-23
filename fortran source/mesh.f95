de9eaa2620612e38f4fe34edc01b38ac6dfcc3d0
LukMarks <lucas.mmoreno@hotmail.com>
2019-12-23 20:47:05 -0300

    script to create the mesh

--- fortran source/mesh.f95
+++ fortran source/mesh.f95
@@ -1,26 +1,33 @@
 ! Author: Lucas Marques Moreno
 ! Date: 12/21/2019 
-! Script capable of apply the finite elemen's
-! algorithm used with the other python scripts
+! Script responsible for the
+! mesh generation
 program mesh
     implicit none
-
+    integer :: n_nodes                                      !number of the nodes
     real :: angle                                           ![degree] temporary angle
     real :: alfa                                            ![degree] angle between two elements
     !real,allocatable,dimension(:) :: nodes                 ![m](x,y) nodes coordenates
-    real, dimension(4,2) :: nodes                           ![m](x,y) nodes coordenates
-    real:: L(3)                                             ![m] length of the element
+    
+    
     integer, parameter :: ikind=selected_real_kind(p=18)    !define precision 
     real(kind = ikind) :: E                                 ![N/m²] Young Modulus
     INTEGER :: row,col,max_rows,max_cols
 
-    open(1, file='inputMech.dat')
-    read(1,*) E
+
+    open(1, file='setup.dat')
+    read(1,*) n_nodes
     close(1)
 
-    open(2, file='inputNodes.dat')
-    read(2,*) nodes
+    real, dimension(n_nodes,2) :: nodes                           ![m](x,y) nodes coordenates
+    real:: L(n_nodes-1)                                                   ![m] length of the element
+    open(2, file='inputMech.dat')
+    read(2,*) E
     close(2)
+
+    open(3, file='inputNodes.dat')
+    read(3,*) nodes
+    close(3)
 
 
     L(1) = sqrt((nodes(2,1)-nodes(1,1))**2+(nodes(2,2)-nodes(1,2))**2)
