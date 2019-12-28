9da22e821b5769b9e5c4895c8e3888c481ef5a0f
LukMarks <lucas.mmoreno@hotmail.com>
2019-12-28 16:01:07 -0300

    add progess information and compile the length progress

--- fortran source/mesh.f95
+++ fortran source/mesh.f95
@@ -9,7 +9,8 @@ program mesh
     integer :: n_elements                                         !number of the nodes
     real :: angle                                           ![degree] temporary angle
     real :: alfa                                            ![degree] angle between two elements 
-    integer :: i
+    real :: x,y,xi,xj,yi,yj                                 ![m] elements coordenates
+    integer :: i,start,end
     real, dimension(:,:),allocatable :: nodes               ![m](x,y) nodes coordenates
     real, dimension(:,:),allocatable :: links               ![m](x,y) nodes coordenates
     real, dimension(:),allocatable :: L               ![m](x,y) nodes coordenates
@@ -21,14 +22,22 @@ program mesh
     print *
     print *, '-------------- Building Mesh --------------'
 
+    print *
+    print *, '-------------- Importing Setup --------------'
+
     open(1, file='setup.dat')
     read(1,*) n_nodes,n_elements
     close(1)
+
+    print *
+    print *, '-------------- Importing Mechanical Properties --------------'
 
     open(2, file='input_young_modulos.dat')
     read(2,*) E
     close(2)
 
+    print *
+    print *, '-------------- Importing Nodes --------------'
 
     allocate ( nodes(n_nodes,2) ) 
 
@@ -37,10 +46,13 @@ program mesh
     close(3)
 
     
-    deallocate(nodes)
+    !deallocate(nodes)
     
     print *, nodes
-    print *, 'links', n_elements
+    print *, 'links', nodes
+
+    print *
+    print *, '-------------- Importing Links --------------'
     
     allocate ( links(n_elements,2) ) 
 
@@ -51,18 +63,36 @@ program mesh
     print *, links
     deallocate(links)
 
+    print *
+    print *, "-------------- Calculating Element's Properties  --------------"
 
     allocate ( L(n_elements) )
     
-    do i=0,n_elements
+    do i=1,n_elements
+        !L(i) = nodes(i,1)
 
-        L(i) = sqrt(( nodes(int(links(i,2)),1) - nodes(int(links(i,1)),1) )**2) !+ ( nodes(int(links(i,2)),2) - nodes( int(links(i,1)) ,2 ) )**2+) 
+        xi = nodes(int(links(i,2)),1)
+        yi = nodes(int(links(i,1)),1)
+
+        xj = nodes(int(links(i,2)),2)
+        yj = nodes(int(links(i,1)),2)
+
+        print *,xi,yi
+        print *
+        print *,xi,yi
+
+        x = xj- xi
+        y = yj-yi
+        print *
+        print *,x,y
+        L(i) = sqrt(x**2+y**2)
+        !L(i) = sqrt(( nodes(int(links(i,2)),1) - nodes(int(links(i,1)),1) )**2) !+ ( nodes(int(links(i,2)),2) - nodes( int(links(i,1)) ,2 ) )**2+) 
 
     end do
 
     print *, L
     deallocate(L)
-
+    deallocate(nodes)
 
 
     print *
