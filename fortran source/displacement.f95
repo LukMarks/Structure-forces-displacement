73910a1d3a8c730b0be9a147ca8fb9854453ae2a
LukMarks <lucas.mmoreno@hotmail.com>
2020-01-06 21:21:30 -0300

    Add the displacement calculation

---
+++ fortran source/displacement.f95
@@ -0,0 +1,10 @@
+program displacement
+    implicit none
+
+    real, dimension(:,:), allocatable :: d                      ![m](x,y) displacement of the nodes 
+    real, dimension(:,:), allocatable :: loads                  ![N](x,y) Loads in each node
+    real, dimension(:,:), allocatable :: k                      ![N/m] stiffness matrix
+        
+
+
+end program displacement
