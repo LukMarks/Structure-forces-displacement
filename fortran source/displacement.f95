3ab9e62acd35f9c4737d791a001f8a64682fc86a
LukMarks <lucas.mmoreno@hotmail.com>
2020-01-13 20:36:54 -0300

    Filter the free nodes stiffness matrix

--- fortran source/displacement.f95
+++ fortran source/displacement.f95
@@ -6,9 +6,10 @@ program displacement
     integer :: i,j,col                                                     !Counters
     real, dimension(:,:), allocatable :: nodes                             ![m](x,y) nodes coordenates
     real, dimension(:,:), allocatable :: d                                 ![m](x,y) displacement of the nodes 
-    real, dimension(:,:), allocatable :: loads                             ![N](x,y) Loads in each node
+    real, dimension(:), allocatable :: load                             ![N](x,y) Loads in each node
+    real, dimension(:), allocatable :: Forces                            ![N](x,y) Loads in the free coordenates
     real, dimension(:,:), allocatable :: k                                 ![N/m] stiffness matrix
-    real, dimension(:,:), allocatable :: links                             !(i,j) Link of each element
+    integer, dimension(:,:), allocatable :: links                             !(i,j) Link of each element
     real, dimension(:), allocatable :: free_nodes                        ![] list of free nodes 
     
 
@@ -40,9 +41,43 @@ program displacement
         READ(4,*) free_nodes(i)    
     END DO
 
+    allocate(k(n_elements*4,n_elements*4))
+    
+    open(5, file='stiffness_matrix.dat')
+    DO i=1,n_elements*4
+        READ(5,*) k(i,:)
+    END DO
+
+    allocate(load(n_nodes*2))
+
+    open(6, file='load.dat')
+    DO i=i,n_nodes*2
+        READ(6,*) load(i)
+    END DO
+
+    !filter only the free nodes
+
+    allocate(Forces(size(free_nodes)))
+
+    DO i=1,size(free_nodes),2
+        Forces(i) = load(links(i,1)*2-1)!Add the force in the x axis
+        Forces(i+1) = load(links(i,1)*2) !Add the force in the y axis
+    END DO
+
+
+
+
+    
+
+
+
     deallocate(nodes)
     deallocate(links)
     deallocate(free_nodes)
+    deallocate(k)
+    deallocate(load)
+    deallocate(Forces)
+    
 
         
 
